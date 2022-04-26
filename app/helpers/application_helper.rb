482f6b48c852f385407abab57ab6df0b5518014a
Russell Norris <sconds@gmail.com>
2022-04-26 17:00:34 -0400

    so much more tweaking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  def fa_icon_tag(*strings, **extras)
+    klass = strings.map do |string|
+      "fa-#{ string }"
+    end
+    tag.i **extras.merge(class: klass)
+  end
+
   def hero_image_tag(hero)
     image_tag @hero.card.variant(resize_to_fill: [200, 300, { crop: :centre }]), class: 'card'
   end
