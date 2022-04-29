c29d0bfcce9385d348305b43773ed0e2cc5483ab
Russell Norris <sconds@gmail.com>
2022-04-29 15:37:57 -0400

    tweak styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def navbar_link_item(text, path)
+    classes = %w[navbar-item]
+    classes << 'is-active' if current_page?(path)
+    link_to text, path, class: classes
+  end
+
   def fa_icon_tag(*strings, **extras)
     klass = strings.map do |string|
       "fa-#{ string }"
